9fa3d037dfec9692e1acaeea7482a1c2c5dc51d7
张新永 <xinyong.zhang1@baozun.com>
2019-09-11 10:34:06 +0800

    feat: npm joi

---
+++ src/pipe/joi-validation-pipe.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { JoiValidationPipePipe } from './joi-validation-pipe.pipe';
+
+describe('JoiValidationPipePipe', () => {
+  it('should be defined', () => {
+    expect(new JoiValidationPipePipe()).toBeDefined();
+  });
+});
