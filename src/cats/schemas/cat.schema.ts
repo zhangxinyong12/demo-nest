1094dd3c10e0a573f0cc52a9b8e3a3eedfb5253d
张新永 <xinyong.zhang1@baozun.com>
2019-09-27 15:32:01 +0800

    fix: collection 表 名

--- src/cats/schemas/cat.schema.ts
+++ src/cats/schemas/cat.schema.ts
@@ -1,7 +1,12 @@
 import * as mongoose from 'mongoose';
 
-export const CatSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
-    breed: String,
-});
+export const CatSchema = new mongoose.Schema(
+    {
+        name: String,
+        age: Number,
+        breed: String,
+    },
+    {
+        collection: 'zhang', // 表 明
+    },
+);
